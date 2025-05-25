717dd80ea6dc81d0264eeee176cfa02ff394c02d
febriaricandra <febriaricandraproduction@gmail.com>
2025-05-25 12:14:06 +0700

    Fix: format auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,84 +3,83 @@ import { AuthService } from './auth.service';
 import { Response } from 'express';
 import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
-
 @ApiTags('auth')
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {}
 
-    @Post('register')
-    @ApiOperation({ summary: 'Register a new user' })
-    @ApiResponse({
-        status: 201,
-        description: 'User registered successfully',
-    })
-    @ApiResponse({
-        status: 400,
-        description: 'Bad Request - Invalid input data',
-    })
-    @ApiResponse({
-        status: 409,
-        description: 'Conflict - User already exists',
-    })
-    @ApiResponse({
-        status: 500,
-        description: 'Internal Server Error',
-    })
-    @ApiBody({
-        type: Object,
-        description: 'User registration data',
-        examples: {
-            example1: {
-                summary: 'Example of user registration data',
-                value: { email: 'example@example.com', password: 'string' }
-            }
-        }
-    })
-    async register(@Body() body: { email: string; password: string }) {
-        return await this.authService.register(body.email, body.password);
-    }
+  @Post('register')
+  @ApiOperation({ summary: 'Register a new user' })
+  @ApiResponse({
+    status: 201,
+    description: 'User registered successfully',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad Request - Invalid input data',
+  })
+  @ApiResponse({
+    status: 409,
+    description: 'Conflict - User already exists',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal Server Error',
+  })
+  @ApiBody({
+    type: Object,
+    description: 'User registration data',
+    examples: {
+      example1: {
+        summary: 'Example of user registration data',
+        value: { email: 'example@example.com', password: 'string' },
+      },
+    },
+  })
+  async register(@Body() body: { email: string; password: string }) {
+    return await this.authService.register(body.email, body.password);
+  }
 
-    @Post('login')
-    @ApiOperation({ summary: 'Login a user' })
-    @ApiResponse({
-        status: 200,
-        description: 'User logged in successfully',
-    })
-    @ApiResponse({
-        status: 401,
-        description: 'Unauthorized - Invalid credentials',
-    })
-    @ApiResponse({
-        status: 500,
-        description: 'Internal Server Error',
-    })
-    @ApiBody({
-        type: Object,
-        description: 'User login data',
-        examples: {
-            example1: {
-                summary: 'Example of user login data',
-                value: { email: 'example@example.com', password: 'string' }
-            }
-        }
-    })
-    async login(@Body() body: { email: string; password: string }) {
-        return await this.authService.login(body.email, body.password);
-    }
+  @Post('login')
+  @ApiOperation({ summary: 'Login a user' })
+  @ApiResponse({
+    status: 200,
+    description: 'User logged in successfully',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized - Invalid credentials',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal Server Error',
+  })
+  @ApiBody({
+    type: Object,
+    description: 'User login data',
+    examples: {
+      example1: {
+        summary: 'Example of user login data',
+        value: { email: 'example@example.com', password: 'string' },
+      },
+    },
+  })
+  async login(@Body() body: { email: string; password: string }) {
+    return await this.authService.login(body.email, body.password);
+  }
 
-    @Post('logout')
-    @ApiOperation({ summary: 'Logout a user' })
-    @ApiResponse({
-        status: 200,
-        description: 'User logged out successfully',
-    })
-    @ApiResponse({
-        status: 500,
-        description: 'Internal Server Error',
-    })
-    logout(@Res() res: Response) {
-        res.clearCookie('jwt');
-        return res.json({ message: 'Logout successful' });
-    }
+  @Post('logout')
+  @ApiOperation({ summary: 'Logout a user' })
+  @ApiResponse({
+    status: 200,
+    description: 'User logged out successfully',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal Server Error',
+  })
+  logout(@Res() res: Response) {
+    res.clearCookie('jwt');
+    return res.json({ message: 'Logout successful' });
+  }
 }
